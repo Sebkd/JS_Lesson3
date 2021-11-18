df0828a225094cc1a7a10aeb42f4fb14b02144cc
Sebkd <sebkd@mail.ru>
2021-11-18 23:10:26 +0300

    end task3

---
+++ task3-app.js
@@ -0,0 +1,24 @@
+'use strict'
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+
+
+products.forEach(function (product) {
+    product.price = product.price - 0.15 * product.price
+    return product
+}
+);
+console.log(products)
