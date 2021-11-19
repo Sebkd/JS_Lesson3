b549134fb9f5e2d1b7ad8bbedba4000b3bc3a756
Sebkd <sebkd@mail.ru>
2021-11-19 22:43:55 +0300

    end task4

--- task4-app.js
+++ task4-app.js
@@ -1,1 +1,49 @@
 'use strict'
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: ["3.jpg"]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+/*
+1. Получить все товары, у которых есть фотографии, можете использовать метод filter
+*/
+
+let userWithPhoto = products.filter(function (product) {
+    try {
+        return product.photos.length != 0;
+    }
+    catch {
+        return;
+    }
+});
+console.log(userWithPhoto);
+
+/*
+2. Отсортируйте товары по цене (от низкой цены к высокой), можете использовать метод sort
+*/
+
+products.sort(function (product1, product2) {
+    if (product1.price < product2.price) return -1;
+    if (product1.price > product2.price) return 1;
+    if (product1.price == product2.price) return 0;
+});
+console.log(products)
